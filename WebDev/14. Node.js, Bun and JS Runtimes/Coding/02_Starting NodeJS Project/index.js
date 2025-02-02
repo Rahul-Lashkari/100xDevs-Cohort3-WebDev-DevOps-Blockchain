68387f36b64d7a8f9be6ed8a14076f30a4f42292
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-02-02 13:17:58 +0530

    Starting NodeJS Project

---
+++ WebDev/14. Node.js, Bun and JS Runtimes/Coding/02_Starting NodeJS Project/index.js
@@ -0,0 +1,15 @@
+/**
+ * You can run this code by using the following command
+ *   $ node index.js
+ *   $ bun index.js
+ *   $ npm run start
+ *   $ npm run bharat
+ */
+
+function sum (a, b) {
+    return a + b;
+}
+
+const ans = sum(1, 2);
+
+console.log(ans);
