ec7187c7efe96e19a12b80df721d29a01d0f8f3f
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-02-09 14:26:03 +0530

    Fetch API using Fetch Method

---
+++ WebDev/18. Headers, Query params and Express/Coding/01_Fetch API using Fetch Method/script.js
@@ -0,0 +1,17 @@
+async function fetchPost() {
+    console.log("Before Sending Request");    
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    
+    const data = await response.json();
+
+    console.log(data);
+
+    console.log("After Receiving Response");
+
+    // document.getElementById("posts").innerText = data.title;
+
+    document.getElementById("posts").innerText = JSON.stringify(data);
+}
+
+fetchPost();
