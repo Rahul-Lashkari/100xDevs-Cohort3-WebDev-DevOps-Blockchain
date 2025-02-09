7b708db8568d73eb132fb5525859ab13418de418
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-02-09 14:50:11 +0530

    Assignment 3 Solution - Request Count
    Created a middleware that counts total number of requests sent to a server. Also an endpoint that exposes it.

---
+++ WebDev/18. Headers, Query params and Express/Assignment Solution/Assignment 3 Solution - Request Count/index.js
@@ -0,0 +1,33 @@
+/*
+Assignment #3 - Create a middleware that counts total number of requests sent to a server. 
+Also create an endpoint that exposes it
+*/
+
+
+const express = require('express');
+
+const app = express();
+
+let requestCount = 0;
+
+function countRequests(req, res, next) {
+    requestCount++;
+
+    next();
+}
+
+app.use(countRequests);
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.get('/requestCount', (req, res) => {
+    res.send({
+        totalRequests: requestCount
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
