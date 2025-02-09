ddbb57b1c769ef67b03776f9f69041f0733b2cb2
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-02-09 14:29:32 +0530

    Fetch API using Axios External Library

---
+++ WebDev/18. Headers, Query params and Express/Coding/02_Fetch API using Axios External Library/script.js
@@ -0,0 +1,17 @@
+async function fetchPost() {
+    console.log("Before Sending Request");    
+
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+
+    const data = response.data;
+
+    console.log(data);
+
+    console.log("After Receiving Response");
+
+    // document.getElementById("posts").innerText = data.title;
+
+    document.getElementById("posts").innerText = JSON.stringify(data);
+}
+
+fetchPost();
