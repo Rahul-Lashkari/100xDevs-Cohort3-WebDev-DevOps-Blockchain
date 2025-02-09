fe24d47a24c3bf86380b24037756acf32c10faf2
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-02-09 14:46:21 +0530

    Assignment 2 Solution
    Created a middleware function that logs each incoming request’s HTTP method, URL, and timestamp to the console.

---
+++ WebDev/18. Headers, Query params and Express/Assignment Solution/Assignment 2 Solution/index.js
@@ -0,0 +1,48 @@
+/*
+Assignment #2 - Create a middleware function that logs each incoming request’s HTTP method, URL, and timestamp to the console.
+*/
+
+
+const express = require('express');
+
+const app = express();
+
+function requestLogger(req, res, next) {
+    const currentTime = new Date();
+
+    /*
+    console.log(req.protocol);
+    console.log(req.host);
+    console.log(req.url);
+    console.log(req.originalUrl);
+    */
+   // Log the HTTP method, Complete URL, and timestamp to the console
+   console.log(req.method);
+    console.log(`${req.protocol}://${req.get('host')}${req.url}`);
+    
+    console.log(currentTime);
+
+    next();
+}
+
+app.use(requestLogger);
+
+app.get('*', (req, res) => {
+    res.send("Hi there!");
+});
+
+app.post('*', (req, res) => {
+    res.send("Hello!");
+});
+
+app.put('*', (req, res) => {
+    res.send("Welcome!");
+});
+
+app.delete('*', (req, res) => {
+    res.send("Goodbye!");
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
