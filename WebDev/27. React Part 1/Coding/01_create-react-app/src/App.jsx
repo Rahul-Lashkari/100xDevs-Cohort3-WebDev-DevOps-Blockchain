41a75131c47fb5fde9f2e8f6281ae874bceb2147
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-14 11:05:50 +0530

    creating a react app

---
+++ WebDev/27. React Part 1/Coding/01_create-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1>React App</h1>
+            <p>Hi there, Welcome to React!</p>
+        </div>
+    );
+}
+
+export default App;
