49ea3c96844cad37ab76227317e8ca8a77a0fac4
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-15 11:56:56 +0530

    Collapsible section

---
+++ WebDev/28. React Part 2/Coding/03_collapsible-section/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <Collapsible title="Section 1">
+                <p>This is the content of section 1.</p>
+            </Collapsible>
+            
+            <Collapsible title="Section 2">
+                <p>This is the content of section 2.</p>
+            </Collapsible>
+        </div>
+    );
+};
+
+const Collapsible = ({ title, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div>
+            <button onClick={() => setIsOpen(!isOpen)}>
+                {title} {isOpen ? '-' : '+'} 
+            </button>
+            
+            {isOpen && <div>{children}</div>}
+        </div>
+    );
+};
+
+export default App;
