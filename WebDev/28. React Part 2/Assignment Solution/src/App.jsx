4d2b1056bc2bf7df44a83a4481bbc2819927a66b
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-15 20:35:21 +0530

    Assignment Solution

---
+++ WebDev/28. React Part 2/Assignment Solution/src/App.jsx
@@ -0,0 +1,14 @@
+import Notifications from "./Notifications";
+
+function App() {
+  
+    return (
+        <div>
+            <h1>Linkedin Notifications Count</h1>
+
+            <Notifications />
+        </div>
+    );
+}
+
+export default App;
