cab490b942671e50af5835ec110c473e26b72041
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-16 22:49:52 +0530

    React Part 3 - useRef hooks

---
+++ WebDev/29. React Part 3 (SPAs, Routing)/Coding/03_useRef-hooks/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useRef } from "react";
+
+function App() {
+    return (
+        <div>
+            <FocusInput />
+            <hr />
+
+            <Stopwatch />
+        </div>
+    );
+}
+
+function FocusInput() {
+    const inputRef = useRef(null);
+
+    function focusOnInput() {
+
+        inputRef.current.focus();
+    }
+
+    return (
+        <div>
+            <h2>Focus Input</h2>
+
+            <input type="text" id="name" ref={inputRef} />
+
+            <button onClick={focusOnInput}>Submit</button>
+        </div>
+    );
+}
+
+function Stopwatch() {
+    const [currentCount, setCurrentCount] = useState(0);
+
+    const timer = useRef(null);
+
+    function startClock() {
+        let value = setInterval(() => {
+            setCurrentCount((count) => count + 1);
+        }, 1000);
+
+        timer.current = value;
+    }
+
+    function stopClock() {
+        console.log(timer);
+
+        clearInterval(timer.current);
+    }
+
+    return (
+        <div>
+            <h1>Stopwatch</h1>
+
+            {currentCount}
+            <br />
+
+            <button onClick={startClock}>Start</button>
+            <button onClick={stopClock}>Stop</button>
+        </div>
+    );
+}
+
+export default App;
