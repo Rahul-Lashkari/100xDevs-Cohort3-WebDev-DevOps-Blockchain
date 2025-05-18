18b9da223aaf101ae081a96b798204fdadf03b8e
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-18 13:01:07 +0530

    Memo in React

---
+++ WebDev/31. React Part 5 (Custom Hooks, Recoil)/Recoil/Coding/04_memo-in-react/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect, memo } from "react";
+
+function App() {
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
+}
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((c) => c + 1); 
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []); 
+
+    return (
+        <div>
+            <CurrentCount count={count} />
+
+            <Increase setCount={setCount} />
+
+            <Decrease setCount={setCount} />
+        </div>
+    );
+}
+
+const CurrentCount = memo(function({ count }) {
+
+    return (
+        <h1>{count}</h1> 
+    );
+});
+
+const Decrease = memo(function({ setCount }) {
+    function decrease() {
+        setCount((c) => c - 1); 
+    }
+
+    return (
+        <button onClick={decrease}>Decrease</button>
+    ); 
+});
+
+const Increase = memo(function({ setCount }) {
+    function increase() {
+        setCount((c) => c + 1); 
+    }
+
+    return (
+        <button onClick={increase}>Increase</button>
+    ); 
+});
+
+export default App;
