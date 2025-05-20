31a0380ec7ae1e05caa846a7f16e5dd4f68141da
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-20 12:40:30 +0530

    Selector Family in Recoil

---
+++ WebDev/31. React Part 5 (Custom Hooks, Recoil)/Recoil Deep Dive/Coding/05_selector-family/src/atoms.js
@@ -0,0 +1,45 @@
+import { atomFamily, selectorFamily } from "recoil";
+
+import axios from "axios";
+
+export const todosAtomFamily = atomFamily({
+    key: 'todosAtomFamily',
+    default: selectorFamily({
+        key: "todoSelectorFamily", 
+
+        get: (id) => async ({ get }) => {
+            const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+
+            return res.data.todo;
+        },
+    })
+});
+
+/*
+export const todosAtomFamily = atomFamily({
+    key: 'todosAtomFamily',
+    default: selectorFamily({
+        key: "todoSelectorFamily", 
+
+        get: function (id) {
+            return async function ({ get }) {
+                const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+
+                return res.data.todo;
+            }
+        },
+    })
+});
+*/
+
+
+// const todo = atom({
+//     key: 'todo',
+//     default: selector({
+//         key: 'todoSelector',
+//         get: async ({ get }) => {
+//             const res = await axios.get('https://sum-server.100xdevs.com/todo');
+//             return res.data.todo;
+//         }
+//     })
+// })
