740dbdbaa37ed229729601e70f6e95fc927c5c5c
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-22 20:22:50 +0530

    checkAge in ts

---
+++ WebDev/33. TypeScript/TypeScript Part 1/Coding/src/04_checkAge.ts
@@ -0,0 +1,11 @@
+// funtion takes in a number and returns a boolean 
+function isLegal(age: number): boolean {
+    if (age > 18) {
+        return true;
+    } else { 
+        return false;
+    }
+}
+
+console.log(isLegal(2));
+console.log(isLegal(20));
