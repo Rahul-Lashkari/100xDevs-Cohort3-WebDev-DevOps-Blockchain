8a2e7dab3857a79819cb3f681bc3f318ea81b52a
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-23 05:13:06 +0530

    Assignment 2 Sol

---
+++ WebDev/33. TypeScript/TypeScript Part 1/Assignment Solution/src/02_assignment2Solution.ts
@@ -0,0 +1,10 @@
+// Write a function that return true or false based on if a user is 18+
+
+function isAdult(age: number): boolean {
+    
+    return age >= 18;
+}
+
+const ans = isAdult(20);
+
+console.log(ans); 
