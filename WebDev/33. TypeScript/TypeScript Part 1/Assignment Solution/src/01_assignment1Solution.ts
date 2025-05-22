8f78b291fe09101a0ed4bc08c74a513dd9f7bde6
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-23 05:11:04 +0530

    Assignment 1 Sol

---
+++ WebDev/33. TypeScript/TypeScript Part 1/Assignment Solution/src/01_assignment1Solution.ts
@@ -0,0 +1,9 @@
+// Assignment #1 - Write a function that calculates the sum of two arguments
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+const ans = sum(5, 10);
+
+console.log(ans);
