9e7055de65456bb51bd08f34457fce698efd165c
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-23 05:18:55 +0530

    Assignment 5 Sol

---
+++ WebDev/33. TypeScript/TypeScript Part 1/Assignment Solution/src/05_assignment5Solution/src/App.tsx
@@ -0,0 +1,21 @@
+import Todo, { TodoType } from "./Todo";
+
+function App() {
+    const todos: TodoType[] = [
+        { title: "Attend 100xDevs Class", description: "In this class we will learn TypeScript.", done: false },
+        { title: "Go to Gym", description: "Let's go gym guys and build your Muscles", done: true },
+        { title: "Write Code", description: "Clean up and improve code quality.", done: false },
+    ];
+
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>Todo List</h1>
+
+            {todos.map((todo, index) => (
+                <Todo key={index} todo={todo} />
+            ))}
+        </div>
+    );
+};
+
+export default App;
