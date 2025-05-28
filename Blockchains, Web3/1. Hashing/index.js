10bbca6dd0f8d4abfaa786f30aec464e1c2e145a
Rahul Lashkari <rahul.lsh.connect@gmail.com>
2025-05-28 23:06:21 +0530

    Intro to hashing

---
+++ Blockchains, Web3/1. Hashing/index.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+const input = "100xdevs";
+const hash = crypto.createHash('sha256').update(input).digest('hex');
+
+console.log(hash)
+
+// output: 55711980d50fd24989409a7a7d4e15fcd206556eec1245c94a6e60910dc4e41d
